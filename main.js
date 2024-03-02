333b7c9a9eeb93253e1a367273a5f3222c3cd61d
Gabriele Corti <borntofrappe@gmail.com>
2024-03-02 17:24:32 +0100

    handle slotted content
    allow to receive svg spritesheets in the custom element
    
    SVG with a given viewBox and direct children describing the individual frames in the same canvas
    
    SVG with a width and height to set the dimensions
    
    on click consider the slotted content or the default svg

--- main.js
+++ main.js
@@ -7,15 +7,17 @@ template.innerHTML = `
     translate: -50% -50%;
   } 
   </style>
-<svg width="40" height="40" viewBox="0 0 100 100">
-  <g>
-    <path style="transform: translate(100px, 0px)" fill="none" stroke="currentColor" stroke-width="5" stroke-linecap="round" stroke-linejoin="round" d="M 48 47 C 49 53 49 53 53 54 49 56 49 56 48 63 47 56 47 56 43 54 47 53 47 53 48 47" />
-    <path style="transform: translate(200px, 0px)" fill="currentColor" stroke="currentColor" stroke-width="5" stroke-linecap="round" stroke-linejoin="round" d="M 48 34 C 51 51 51 51 63 53 50 56 50 56 49 71 46 56 46 56 34 54 45 50 45 50 48 34 M 38 43 Z M 60 60 Z M 56 65 Q 57 66 56 67 55 66 56 65" />
-    <path style="transform: translate(300px, 0px)" fill="currentColor" stroke="currentColor" stroke-width="5" stroke-linecap="round" stroke-linejoin="round" d="M 46 26 C 51 48 51 48 70 51 51 55 51 55 47 78 43 55 43 55 25 51 42 47 42 47 46 26 M 61 67 Z M 34 40 Q 32 41 32 38 34 38 34 40" />
-    <path style="transform: translate(400px, 0px)" fill="none" stroke="currentColor" stroke-width="5" stroke-linecap="round" stroke-linejoin="round" d="M 44 28 43 15 M 73 50 82 49 M 47 67 48 82 M 26 51 13 52 M 61 69 Z M 23 36 Z" />
-    <path style="transform: translate(500px, 0px)" fill="none" stroke="currentColor" stroke-width="5" stroke-linecap="round" stroke-linejoin="round" d="M 43 17 42 13 M 83 48 88 47 M 50 82 51 87 M 12 53 6 54" />
-  </g>
-</svg>
+<slot>
+  <svg width="40" height="40" viewBox="0 0 100 100">
+    <g>
+      <path style="transform: translate(100px, 0px)" fill="none" stroke="currentColor" stroke-width="5" stroke-linecap="round" stroke-linejoin="round" d="M 48 47 C 49 53 49 53 53 54 49 56 49 56 48 63 47 56 47 56 43 54 47 53 47 53 48 47" />
+      <path style="transform: translate(200px, 0px)" fill="currentColor" stroke="currentColor" stroke-width="5" stroke-linecap="round" stroke-linejoin="round" d="M 48 34 C 51 51 51 51 63 53 50 56 50 56 49 71 46 56 46 56 34 54 45 50 45 50 48 34 M 38 43 Z M 60 60 Z M 56 65 Q 57 66 56 67 55 66 56 65" />
+      <path style="transform: translate(300px, 0px)" fill="currentColor" stroke="currentColor" stroke-width="5" stroke-linecap="round" stroke-linejoin="round" d="M 46 26 C 51 48 51 48 70 51 51 55 51 55 47 78 43 55 43 55 25 51 42 47 42 47 46 26 M 61 67 Z M 34 40 Q 32 41 32 38 34 38 34 40" />
+      <path style="transform: translate(400px, 0px)" fill="none" stroke="currentColor" stroke-width="5" stroke-linecap="round" stroke-linejoin="round" d="M 44 28 43 15 M 73 50 82 49 M 47 67 48 82 M 26 51 13 52 M 61 69 Z M 23 36 Z" />
+      <path style="transform: translate(500px, 0px)" fill="none" stroke="currentColor" stroke-width="5" stroke-linecap="round" stroke-linejoin="round" d="M 43 17 42 13 M 83 48 88 47 M 50 82 51 87 M 12 53 6 54" />
+    </g>
+  </svg>
+</slot>
 `;
 
 class FrameByFrameSVG extends HTMLElement {
@@ -25,6 +27,7 @@ class FrameByFrameSVG extends HTMLElement {
     this.attachShadow({ mode: "open" });
 
     this.handlePointer = this.handlePointer.bind(this);
+    this.handleSlotchange = this.handleSlotchange.bind(this);
   }
 
   handlePointer(e) {
@@ -34,7 +37,12 @@ class FrameByFrameSVG extends HTMLElement {
     host.style.setProperty("left", `${left}px`);
     host.style.setProperty("top", `${top}px`);
 
-    const svg = shadowRoot.querySelector("svg");
+    const svg =
+      shadowRoot
+        .querySelector("slot")
+        .assignedElements()
+        .find((d) => d.tagName === "svg") || shadowRoot.querySelector("svg");
+
     const group = svg.querySelector("g");
 
     const { width } = svg.viewBox.baseVal;
@@ -64,9 +72,39 @@ class FrameByFrameSVG extends HTMLElement {
     group.animate(keyframes, options);
   }
 
+  handleSlotchange() {
+    const { shadowRoot } = this;
+
+    const svg = shadowRoot
+      .querySelector("slot")
+      .assignedElements()
+      .find((d) => d.tagName === "svg");
+
+    if (svg) {
+      const group = document.createElementNS("http://www.w3.org/2000/svg", "g");
+
+      const { width } = svg.viewBox.baseVal;
+
+      [...svg.children].forEach((child, i) => {
+        child.style.setProperty(
+          "transform",
+          `translate(${width * (i + 1)}px, 0px)`
+        );
+
+        const orphan = child.parentNode.removeChild(child);
+        group.appendChild(orphan);
+      });
+
+      svg.appendChild(group);
+    }
+  }
+
   connectedCallback() {
     const { shadowRoot } = this;
     shadowRoot.appendChild(template.content.cloneNode(true));
+
+    const slot = shadowRoot.querySelector("slot");
+    slot.addEventListener("slotchange", this.handleSlotchange);
 
     document.documentElement.addEventListener(
       "pointerdown",
